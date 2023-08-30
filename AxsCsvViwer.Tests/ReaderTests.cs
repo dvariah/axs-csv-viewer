a02e2587c47a3119e847b6c554f2275d4eea37fb
dvariah <denys_variah@epam.com>
2023-08-30 17:05:13 +0300

    Add unit tests for reader

---
+++ AxsCsvViwer.Tests/ReaderTests.cs
@@ -0,0 +1,25 @@
+﻿using AxsCsvViewer.Services;
+
+namespace AxsCsvViwer.Tests
+{
+    [TestFixture]
+    internal class ReaderTests
+    {
+        private IReader _sut;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _sut = new Reader();
+        }
+
+        [Test]
+        public void Read_HappyPath()
+        {
+            var result = _sut.Read("./Data/testSale.csv");
+            Assert.AreEqual("941670467:21170491", result.First().transaction_id);
+            Assert.AreEqual("FIRSTFIELD", result.Last().unique_id);
+            Assert.AreEqual("LASTFIELD", result.Last().merch_component_name);
+        }
+    }
+}
