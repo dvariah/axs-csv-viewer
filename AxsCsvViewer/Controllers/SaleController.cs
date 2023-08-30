4aba3c0ef9e329f5315d93fb0d07f1e5b1503408
dvariah <denys_variah@epam.com>
2023-08-30 12:51:21 +0300

    Add sale controller

---
+++ AxsCsvViewer/Controllers/SaleController.cs
@@ -0,0 +1,35 @@
+﻿using AxsCsvViewer.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AxsCsvViewer.Controllers
+{
+    public class SaleController : Controller
+    {
+        ISaleService _saleService;
+
+        public SaleController(ISaleService service)
+        {
+            _saleService = service;
+        }
+
+        /// <summary>
+        /// Returns a collection of unique transaction ids from sales csv
+        /// </summary>
+        /// <param name="subscription"></param>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<IActionResult> GetAllUniqueTansaction()
+        {
+            var result = await _saleService.GetAllUniqueTansactionsId();
+
+            return Ok(result);
+        }
+
+        public async Task<IActionResult> GetByTansactionId()
+        {
+            var result = await _saleService.GetSalesByTansactionId();
+
+            return Ok(result);
+        }
+    }
+}
