18a6346b2c03f53fff6b2a3d0d9770826138c896
dvariah <denys_variah@epam.com>
2023-08-30 17:03:49 +0300

    Inject dependecies

--- AxsCsvViewer/Program.cs
+++ AxsCsvViewer/Program.cs
@@ -1,9 +1,14 @@
+using AxsCsvViewer.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton<IReader, Reader>();
+builder.Services.AddSingleton<ISaleService, SaleService>();
 
 var app = builder.Build();
 
